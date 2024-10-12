9607e4712b73198fef79367275d3aaf0c75a346d
rafal <zebraf.dev@gmail.com>
2024-10-13 01:25:26 +0200

    feat: add vimade

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,28 +4,37 @@
 -- See the kickstart.nvim README for more information
 return {
   -- {
-  --   'TaDaa/vimade',
+  --   'sunjon/shade.nvim',
   --   config = function()
-  --     vim.g.vimade = {
-  --       normalid = '',
-  --       normalncid = '',
-  --       basefg = '',
-  --       basebg = '',
-  --       fadelevel = 0.7,
-  --       detecttermcolors = 0,
-  --       signsid = 13100,
-  --       signsretentionperiod = 4000,
-  --       fademinimap = 1,
-  --       fadepriority = 10,
-  --       groupdiff = 1,
-  --       groupscrollbind = 0,
-  --       enablefocusfading = 0,
-  --       enablebasegroups = 1,
-  --       basegroups = {
-  --         'Folded', 'Search', 'SignColumn', 'LineNr', 'CursorLine', 'CursorLineNr',
-  --         'DiffAdd', 'DiffChange', 'DiffDelete', 'DiffText', 'FoldColumn', 'Whitespace'
-  --       }
-  --     }
+  --     require 'shade'.setup({
+  --       overlay_opacity = 65,
+  --       opacity_step = 1,
+  --     })
   --   end
-  -- }
+  -- },
+  {
+    'TaDaa/vimade',
+    config = function()
+      vim.g.vimade = {
+        normalid = '',
+        normalncid = '',
+        basefg = '',
+        basebg = '',
+        fadelevel = 0.6,
+        detecttermcolors = 0,
+        signsid = 13100,
+        signsretentionperiod = 4000,
+        fademinimap = 1,
+        fadepriority = 10,
+        groupdiff = 1,
+        groupscrollbind = 0,
+        enablefocusfading = 0,
+        enablebasegroups = 1,
+        basegroups = {
+          'Folded', 'Search', 'SignColumn', 'LineNr', 'CursorLine', 'CursorLineNr',
+          'DiffAdd', 'DiffChange', 'DiffDelete', 'DiffText', 'FoldColumn', 'Whitespace'
+        }
+      }
+    end
+  }
 }
